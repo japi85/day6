444ecf5b7f60cdbed1afd138fa80f00a2a405db0
japi85 <jani.o.piiroinen@gmail.com>
2021-10-19 10:27:49 +0300

    added end 2 points

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('endpoint / response');
 });
 
+router.get('/first', function(req, res, next) {
+  res.send('server responds: endpoint eka');
+});
+
+//
+router.get('/second/:fname', function(request, response){
+  
+  console.log("Firstname= " + request.params.fname);
+  response.send("Hello, " + request.params.fname);
+
+});
 module.exports = router;
